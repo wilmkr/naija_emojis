5ec97143d0dbc2c1b9efe1c9bfe6e790c263e100
Wilson Omokoro <wilson.omokoro@andela.com>
2015-12-18 15:54:18 +0100

    (fix) fix bug in Configuration.php

--- src/Configuration.php
+++ src/Configuration.php
@@ -8,7 +8,14 @@ class Configuration
 {
     public static function load()
     {
-        $dotenv = new Dotenv($_SERVER['DOCUMENT_ROOT']);
-        $dotenv->load();
+        // $dotenv = new Dotenv($_SERVER['DOCUMENT_ROOT']);
+        // $dotenv->load();
+
+        if (! getenv('APP_ENV') || getenv('APP_ENV')=="local") {
+            // load config values from .env file if APP_ENV is not found.
+            // APP_ENV is set on Heroku server
+            $dotenv = new Dotenv($_SERVER['DOCUMENT_ROOT']);
+            $dotenv->load();
+        }
     }
 }
