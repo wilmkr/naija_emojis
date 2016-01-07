f837d75236e8294f3a2f9be916124982b83562ad
Wilson Omokoro <wilson.omokoro@andela.com>
2016-01-07 12:12:31 +0100

    (fix) Set client URLin tests

--- tests/EmojiControllerTest.php
+++ tests/EmojiControllerTest.php
@@ -15,8 +15,7 @@ class EmojiControllerTest extends PHPUnit_Framework_TestCase
 
     public function setup()
     {
-        //$this->client = new Client(['base_uri' => 'https://w-naija-emoji.herokuapp.com']);
-        $this->client = new Client(['base_uri' => 'http://checkpoint3.app']);
+        $this->client = new Client(['base_uri' => 'https://w-naija-emoji.herokuapp.com']);
 
         $response = $this->client->post('/auth/login', ['query' => ['username' => 'Wil', 'password' => 'password']]);
 
